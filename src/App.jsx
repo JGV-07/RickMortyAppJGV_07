2dfcbddcb08429efcd0faaa3f7c978e6592b53e8
Jonathan Garcia <133997935+JGV-07@users.noreply.github.com>
2023-07-07 00:33:33 -0500

    maquetado de las cards y logica de las cards random

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
+import getRandomNumber from './utils/getRandomNumber'
+import LocationInfo from './components/LocationInfo'
+import ResidentCard from './components/ResidentCard'
 
 function App() {
 
+  const [location, setLocation] = useState()
 
+  useEffect(() => {
+    const url = `https://rickandmortyapi.com/api/location/${getRandomNumber(126)}`
+    axios.get(url)
+      .then(res => setLocation(res.data))
+      .catch(err => console.error(err))
+
+  }, [])
+  
   return (
     <div>
       <h1>Rick And Morty App</h1>
+      <LocationInfo
+        location={location}
+      />
+      <div className='resident-container'>
+        {
+          location?.residents.map(url =>(
+            <ResidentCard 
+            key={url}
+            url={url}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
---
+++ src/utils/getRandomNumber.js
@@ -0,0 +1,7 @@
+const getRandomNumber = (limit) => {
+
+    return Math.floor(Math.random() * limit) +1
+}
+export default getRandomNumber
+
+//Numero de 1 hasta el limite
